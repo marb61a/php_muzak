eeb626b920b89ce3e000064a855ec7b9e351c41f
marb61a <marb61a@gmail.com>
2017-11-27 17:48:09 +0000

    added register form

--- register.php
+++ register.php
@@ -1,3 +1,11 @@
+<?php 
+	include("includes/classes/Account.php");
+
+	$account = new Account();
+	$account->register();
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,6 +25,39 @@
 			</p>
 
 			<button type="submit" name="loginButton">Log In</button>
+		</form>
+
+		<form id="registerForm" action="register.php" method="POST">			
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+			</p>
+			<p>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+			</p>
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">Register</button>
 		</form>
 	</div>
 </body>
