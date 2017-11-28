3f0ac6653dc24c72e96d3a0581a492cc9dbedf17
marb61a <marb61a@gmail.com>
2017-11-28 17:08:49 +0000

    updated Account php file

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -2,22 +2,42 @@
 
 	class Account
 	{
+		private $errorArray;
 		
 		public function __construct()
 		{
+			$this->errorArray = array();
+		}
+
+		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2){
+			$this->validateUsername($un);
+			$this->validateFirstName($fn);
+			$this->validateLastName($ln);
+			$this->validateEmails($em, $em2);
+			$this->validatePasswords($pw, $pw2);
+
+			if (empty($this->errorArray) == true) {
+				return true;
+			} else {
+				return false;
+			}
 			
 		}
 
-		public function register(){
-			$this->validateUsername($username);
-			$this->validateFirstName($firstName);
-			$this->validateLastName($lastName);
-			$this->validateEmails($email, $email2);
-			$this->validatePasswords($password, $password2);
-		}
+		public function getError($error){
+			if (!in_array($error, $this->errorArray)) {
+				$error = "";
+			}
+
+			return "<span class='errorMessage'>$error</span>";
+		}	
 
 		private function validateUsername($un){
-			echo "username function called";
+			if (strlen($un) > 25 || strlen($un) < 5) {
+				array_push($this->errorArray, "Your username must be between 5 and 25 characters")
+
+				return;
+			}
 		}
 
 		private function validateFirstName($fn){
