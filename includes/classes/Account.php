d623af14708f635bb1d53f1a7f5dbe8b3ebf2497
marb61a <marb61a@gmail.com>
2017-11-30 16:39:32 +0000

    updated account.php functions

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -41,19 +41,53 @@
 		}
 
 		private function validateFirstName($fn){
+			if (strlen($fn) > 25 || strlen($fn) < 2) {
+				array_push($this->errorArray, "Your first name must be between 2 and 25 characters")
 
+				return;
+			}
 		}
 
 		private function validateLastName($ln){
+			if (strlen($ln) > 25 || strlen($ln) < 5) {
+				array_push($this->errorArray, "Your last name must be between 2 and 25 characters")
 
+				return;
+			}
 		}
 
 		private function validateEmails($em, $em2) {
-			
+			if($em != $em2){
+				array_push($this->errorArray, "Your email addresses must match");
+
+				return;	
+			}
+
+			if(!filter_var($em, FILTER_VALIDATE_EMAIL)){
+				array_push($this->errorArray, "Your email adress is invalid");
+
+				return;
+			}
 		}
 
 		private function validatePasswords($pw, $pw2) {
-			
+			if($pw != $pw2){
+				array_push($this->errorArray, "Your passwords must match");
+
+				return;
+			}
+
+			if(preg_match('//[^A-Za-z0-9]', $pw)){
+				array_push($this->errorArray, "Your passwords must contain only numbers and letters");
+
+				return;
+			}
+
+			if(strlen($pw) > 30 || strlen($pw) < 5){
+				array_push($this->errorArray, "Your password must be between 5 and 30 characters");
+
+				return;
+			}
 		}
 	}
 ?>
