6b466f5629ff07d50b0882ca81fc0c66b075bccd
marb61a <marb61a@gmail.com>
2018-01-04 17:18:01 +0000

    updated index.php with nowplayingcontainer

--- index.php
+++ index.php
@@ -31,7 +31,46 @@
 							</a>
 						</div>
 					</div>
+					<div class="group">
+						<div class="navItem">
+							<a href="browse.php" class="navItemLink">Browse</a>
+						</div>
+
+						<div class="navItem">
+							<a href="yourMusic.php" class="navItemLink">Your Music</a>
+						</div>
+
+						<div class="navItem">
+							<a href="profile.php" class="navItemLink">Joe Bloggs</a>
+						</div>
+					</div>
 				</nav>
+			</div>
+		</div>
+		<div id="nowPlayingBarContainer">
+			<div id="nowPlayingBar">
+				<div id="nowPlayingLeft">
+					<div class="content">
+						<span class="albumLink">
+							<img src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" class="albumArtwork">
+						</span>
+						<div class="trackInfo">
+							<span class="trackName">
+								<span>Blah blah</span>
+							</span>
+							<span class="artistName">
+								<span>Joe Bloggs</span>
+							</span>
+						</div>
+					</div>
+				</div>
+				<div id="nowPlayingCenter">
+					<div class="content playerControls">
+						<div class="buttons">
+							
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
