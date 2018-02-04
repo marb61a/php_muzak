478362d4d707b5e91c976724e69551bc1dbea1ca
marb61a <marb61a@gmail.com>
2018-02-04 18:21:20 +0000

    updated script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,32 @@ $(document).click(function(click){
 $(window).scroll(function() {
 	hideOptionsMenu();
 });
+
+$(document).on("change", "select.playlist", function(){
+	var select = $(this);
+	var playlistId = select.val();
+	var songId = select.prev(".songId").val();
+
+	$.post("includes/handlers/ajax/addToPlaylist.php", {
+		playlistId: playlistId,
+		songId: songId
+	})
+	.done(function(error){
+		if(error != ""){
+			alert(error);
+			return;
+		}
+
+		hideOptionsMenu();
+		select.val("");
+	})
+});
+
+function updateEmail(emailClass){
+	var emailValue = $("." + emailClass).val();
+
+	$.post("includes/handlers/ajax/updateEmail.php", { 
+		email: emailValue, 
+		username: userLoggedIn
+	})
+}
