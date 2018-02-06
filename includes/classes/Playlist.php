9a492dc53e0fb0bce799be1b4ec3a4aea0c16010
marb61a <marb61a@gmail.com>
2018-02-06 12:06:23 +0000

    added playlist.php

---
+++ includes/classes/Playlist.php
@@ -0,0 +1,32 @@
+<?php
+	class Playlist {
+		private $con;
+		private $id;
+		private $name;
+		private $owner;
+
+		public function __construct($con, $data){
+			if(!is_array($data)){
+				$query = mysqli_query($con, "SELECT * FROM playlists WHERE id='$data'");
+				$data = mysqli_fetch_array($query);
+			}
+
+			$this->con = $con;
+			$this->id = $data['id'];
+			$this->name = $data['name'];
+			$this->owner = $data['owner'];
+		}
+
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getName() {
+			return $this->name;
+		}
+
+		public function getOwner() {
+			return $this->owner;
+		}
+	}
+?>
