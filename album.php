7802466c3c3e7e6fd2a7d9a74aff3f2a8e2de609
marb61a <marb61a@gmail.com>
2018-02-21 18:34:15 +0000

    updated album.php

--- album.php
+++ album.php
@@ -25,7 +25,40 @@
 <div class="tracklistContainer">
 	<ul class="tracklist">
 		<?php
+			$songIdArray = $album->getSongIds();
 
+			$i = 1;
+			foreach ($$songIdArray as $songId) {
+				$albumSong = new Song($con, $songId);
+				$albumArtist = $albumSong->getArtist();
+
+				echo "
+					<li class='tracklistRow'>
+						<div class='trackCount'>
+							<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
+							<span class='trackNumber'>$i</span>
+						</div>
+						<div class='trackOptions'>
+							<input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+							<img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
+						</div>
+						<div class='trackDuration'>
+							<span class='duration'>" . $albumSong->getDuration() . "</span>
+						</div>
+					</li>
+				";
+				$i = Si + 1;
+			}
 		?>
+
+		<script>
+			var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+		</script>
 	</ul>
 </div>
+
+<nav class="optionsMenu">
+	<input type="hidden" class="songId">
+	<?php echo Playlist::getPlaylistsDropdown($con, $userLoggedIn->getUsername());?>
+</nav>
