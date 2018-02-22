617efd77c3561e5a121d555559dc48d6efa7bbf5
marb61a <marb61a@gmail.com>
2018-02-22 16:50:05 +0000

    updated script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,4 +81,33 @@ function openPage(url){
 		url = url + "?";
 	}
 	
+	var encodedUrl = encodeURI(url + "&userLoggedIn=" + userLoggedIn);
+	$("#mainContent").load(encodedUrl);
+	$("body").scrollTop(0);
+	history.pushState(null, null, url);
+}
+
+function removeFromPlaylist(button, playlistId){
+	var songId = $(button).prevAll(".songId").val();
+	$.post("includes/handlers/ajax/removeFromPlaylist.php", {
+		playlistId: playlistId,
+		songId: songId
+	})
+		.done(function(error){
+			if(error != ""){
+				alert(error);
+				return;
+			}
+
+			//do something when ajax returns
+			openPage("playlist.php?id=" + playlistId);
+		});
+}
+
+function createPlaylist(){
+	var popup = prompt("Please enter the name of your playlist");
+
+	if(popup != null){
+		
+	}
 }
