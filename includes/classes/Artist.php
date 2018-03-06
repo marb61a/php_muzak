f9aa7dfaf63f2638990183a16aa92c63a4045ba0
marb61a <marb61a@gmail.com>
2018-03-06 12:07:28 +0000

    updated Song.php and added Artist.php

---
+++ includes/classes/Artist.php
@@ -0,0 +1,11 @@
+<?php
+	class Artist {
+		private $con;
+		private $id;
+
+		public function __construct($con, $id){
+			$this->con = $con;
+			$this->id = $id;
+		}
+	}
+?>
--- includes/classes/Song.php
+++ includes/classes/Song.php
@@ -23,5 +23,38 @@
 			$this->duration = $this->mysqliData['duration'];
 			$this->path = $this->mysqliData['path'];
 		}
+
+		public function getTitle() {
+			return $this->title;
+		}
+
+		public function getId() {
+			return $this->id;
+		}
+
+		public function getArtist() {
+			return new Artist($this->con, $this->artistId);
+		}
+
+		public function getAlbum() {
+			return new Album($this->con, $this->albumId);
+		}
+
+		public function getPath() {
+			return $this->path;
+		}
+
+		public function getDuration() {
+			return $this->duration;
+		}
+
+		public function getMysqliData() {
+			return $this->mysqliData;
+		}
+
+		public function getGenre() {
+			return $this->genre;
+		}
+
 	}
 ?>
