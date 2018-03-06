72bfecbbda6f44f2ebbdd160319c043c3fd65b9d
marb61a <marb61a@gmail.com>
2018-03-06 12:21:15 +0000

    updated Artist.php and added getAlbumJson.php

--- includes/classes/Artist.php
+++ includes/classes/Artist.php
@@ -7,5 +7,27 @@
 			$this->con = $con;
 			$this->id = $id;
 		}
+
+		public function getId(){
+			return $this->id;
+		}
+
+		public function getName(){
+			$artistQuery = mysqli_query($this->con, "SELECT name FROM artists WHERE id='$this->id'");
+			$artist = mysqli_fetch_array($artistQuery);
+			return $artist['name'];
+		}
+
+		public function getSongIds(){
+			$query = mysqli_query($this->con, "SELECT id FROM songs WHERE artist='$this->id' ORDER BY plays ASC");
+			$array = array();
+
+			while ($row = mysqli_fetch_array($query)) {
+				array_push($array, $row['id']);
+			}
+
+			return $array;
+		}
+
 	}
 ?>
