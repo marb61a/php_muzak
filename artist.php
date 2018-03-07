caa313e97a490837753e8e9fabb2765fd18fec7b
marb61a <marb61a@gmail.com>
2018-03-07 16:48:12 +0000

    updated artist.php

--- artist.php
+++ artist.php
@@ -29,7 +29,43 @@
 	<h2>SONGS</h2>
 	<ul class="tracklist">
 		<?php
+			$songIdArray = $artist->getSongIds();
 
+			$i = 1;
+			foreach ($songIdArray as $songId) {
+				if($i > 5){
+					break;
+				}
+
+				$albumSong = new Song($con, $songId);
+				$albumArtist = $albumSong->getArtist();
+
+				echo "<li class='tracklistRow'>
+					<div class='trackCount'>
+						<img class='play' src='assets/images/icons/play-white.png' onclick='setTrack(\"" . $albumSong->getId() . "\", tempPlaylist, true)'>
+						<span class='trackNumber'>$i</span>
+					</div>
+
+					<div class='trackInfo'>
+						<span class='trackName'>" . $albumSong->getTitle() . "</span>
+						<span class='artistName'>" . $albumArtist->getName() . "</span>
+					</div>
+
+					div class='trackOptions'>
+						<input type='hidden' class='songId' value='" . $albumSong->getId() . "'>
+						<img class='optionsButton' src='assets/images/icons/more.png' onclick='showOptionsMenu(this)'>
+					</div>
+
+					<div class='trackDuration'>
+						<span class='duration'>" . $albumSong->getDuration() . "</span>
+					</div>
+				</li>";	
+			}
 		?>
+
+		<script>
+			var tempSongIds = '<?php echo json_encode($songIdArray); ?>';
+			tempPlaylist = JSON.parse(tempSongIds);
+		</script>
 	</ul>
 </div>
