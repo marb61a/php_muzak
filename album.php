9b7f025d9be5ee6067672e355437ffde7da58ecf
marb61a <marb61a@gmail.com>
2018-03-11 18:26:03 +0000

    added yourmusic

--- album.php
+++ album.php
@@ -19,6 +19,9 @@
 
 	<div class="rightSection">
 		<h2><?php echo $album->getTitle(); ?></h2>
+		<p role="link" tabindex="0" onclick="openPage('artist.php?id=<?php echo $artistId; ?>')">
+			By <?php echo $artist->getName(); ?></p>
+		<p><?php echo $album->getNumberOfSongs(); ?> songs</p>
 	</div>
 </div>
 
--- yourMusic.php
+++ yourMusic.php
@@ -10,5 +10,31 @@
 				NEW PLAYLIST
 			</button>
 		</div>
+
+		<?php
+			$username = $userLoggedIn->getUsername();
+
+			$playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
+
+			if(mysqli_num_rows($playlistsQuery) == 0) {
+				echo "<span class='noResults'>You don't have any playlists yet.</span>";
+			}
+
+			while($row = mysqli_fetch_array($playlistsQuery)) {
+
+				$playlist = new Playlist($con, $row);
+
+				echo "<div class='gridViewItem' role='link' tabindex='0' 
+							onclick='openPage(\"playlist.php?id=" . $playlist->getId() . "\")'>
+						<div class='playlistImage'>
+							<img src='assets/images/icons/playlist.png'>
+						</div>
+						
+						<div class='gridViewInfo'>"
+							. $playlist->getName() .
+						"</div>
+					</div>";
+			}
+		?>
 	</div>
 </div>
