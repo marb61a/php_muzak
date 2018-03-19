9ac1cf8520990d96bd6e66b2e8955cae589c0913
martin byrne <marb61a@gmail.com>
2018-03-19 13:44:19 +0000

    updated login hadler and email change handler

--- includes/classes/Account.php
+++ includes/classes/Account.php
@@ -1,5 +1,6 @@
 <?php
 	class Account {
+
 		private $con;
 		private $errorArray;
 
@@ -9,16 +10,19 @@
 		}
 
 		public function login($un, $pw) {
+
 			$pw = md5($pw);
 
 			$query = mysqli_query($this->con, "SELECT * FROM users WHERE username='$un' AND password='$pw'");
 
 			if(mysqli_num_rows($query) == 1) {
 				return true;
-			} else {
+			}
+			else {
 				array_push($this->errorArray, Constants::$loginFailed);
 				return false;
 			}
+
 		}
 
 		public function register($un, $fn, $ln, $em, $em2, $pw, $pw2) {
@@ -31,7 +35,8 @@
 			if(empty($this->errorArray) == true) {
 				//Insert into db
 				return $this->insertUserDetails($un, $fn, $ln, $em, $pw);
-			} else {
+			}
+			else {
 				return false;
 			}
 
@@ -41,7 +46,6 @@
 			if(!in_array($error, $this->errorArray)) {
 				$error = "";
 			}
-
 			return "<span class='errorMessage'>$error</span>";
 		}
 
@@ -56,6 +60,7 @@
 		}
 
 		private function validateUsername($un) {
+
 			if(strlen($un) > 25 || strlen($un) < 5) {
 				array_push($this->errorArray, Constants::$usernameCharacters);
 				return;
@@ -66,6 +71,7 @@
 				array_push($this->errorArray, Constants::$usernameTaken);
 				return;
 			}
+
 		}
 
 		private function validateFirstName($fn) {
@@ -98,6 +104,7 @@
 				array_push($this->errorArray, Constants::$emailTaken);
 				return;
 			}
+
 		}
 
 		private function validatePasswords($pw, $pw2) {
@@ -117,5 +124,6 @@
 				return;
 			}
 		}
+		
 	}
 ?>
