87343cbe0096031f13398b26e495c0315b18ace8
martin byrne <marb61a@gmail.com>
2018-03-20 13:44:44 +0000

    update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -175,3 +175,35 @@ function formatTime(seconds){
 
 	return minutes + ":" + extraZero + seconds;
 }
+
+function updateTimeProgressBar(audio){
+	$(".progressTime.current").text(formatTime(audio.currentTime));
+	$(".progressTime.remaining").text(formatTime(audio.duration - audio.currentTime));
+	
+	var progress = audio.currentTime / audio.duration * 100;
+	$(".playbackBar .progress").css("width", progress + "%");
+}
+
+function updateVolumeProgressBar(audio){
+	var volume = audio.volume * 100;
+	$(".volumeBar .progress").css("width", volume + "%");
+}
+
+function playFirstSong(){
+	setTrack(tempPlaylist[0], tempPlaylist, true);
+}
+
+function Audio(){
+	this.currentlyPlaying;
+	this.audio = document.createElement('audio');
+	
+	this.audio.addEventListener("ended", function() {
+	    nextSong();
+	});
+	
+	this.audio.addEventListener("canplay", function(){
+		//'this' refers to the object that the event was called on
+		var duration = formatTime(this.duration);
+		$(".progressTime.remaining").text(duration);	
+	});
+}
