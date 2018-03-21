10feb43cdf3b29764a4a6d17d922620f36d4a34b
martin byrne <marb61a@gmail.com>
2018-03-21 13:19:59 +0000

    updated style.css

--- search.php
+++ search.php
@@ -115,7 +115,7 @@
 			echo "<span class='noResults'>No albums found matching " . $term . "</span>";
 		}
 
-		while () {
+		while($row = mysqli_fetch_array($albumQuery)){
 			echo "<div class='gridViewItem'>
 				<span role='link' tabindex='0' onclick='openPage(\"album.php?id=" . $row['id'] . "\")'> 
 					<img src='" . $row['artworkPath'] . "'>
